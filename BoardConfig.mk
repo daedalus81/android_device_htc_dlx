74e4546fc74b006221d0f4ba1b36419a548d3a6a
Michael Gernoth <michael@gernoth.net>
2015-12-14 17:50:00 -0600

    ril: Return static radio capability from getRadioCapability
    Change-Id: Ifbe739ce447ac1eadbe03139326a148e4b4169d1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,7 @@ BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
+BOARD_RIL_CLASS := ../../../device/htc/dlx/ril
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk

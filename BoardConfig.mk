eb54007204db7e8055cc1214b4133798913722df
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-08-31 14:57:29 -0500

    dlx: use android/kernel/dlx
    * m7 is not using this kernel anymore, dlx is no longer supported,
      just switch to its own kernel name
    
    Change-Id: I789c3c4aae3c97e7948e46b42c60269c2e11448b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=dlx user_debug=0
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := cyanogenmod_dlx_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-TARGET_KERNEL_SOURCE := kernel/htc/m7
+TARGET_KERNEL_SOURCE := kernel/htc/dlx
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true

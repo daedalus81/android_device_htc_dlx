0141eede60a46c64c5a52c1a2e0bc6df591268d7
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-09-01 10:16:54 -0500

    dlx: change BOARD_BLUEDROID_VENDOR_CONF to BOARD_CUSTOM_BT_CONFIG

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ HAVE_ADRENO_SOURCE := false
 # Bluetooth
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/dlx/bluetooth/include
-BOARD_BLUEDROID_VENDOR_CONF := device/htc/dlx/bluetooth/vnd_dlx.txt
+BOARD_CUSTOM_BT_CONFIG := device/htc/dlx/bluetooth/vnd_dlx.txt
 BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
 # Radio

44e26adf4fc87edcbcbc4f9c31f4a79bcb001242
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 03:36:00 -0600

    dlx: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- cm.mk
+++ cm.mk
@@ -11,9 +11,6 @@ PRODUCT_PACKAGE_OVERLAYS += device/htc/dlx/overlay
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/htc/dlx/full_dlx.mk)
 
